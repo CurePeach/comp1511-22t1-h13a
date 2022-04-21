86d6fcddd13c4f4151be911bc5fff7dab019dd98
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2022-04-21 19:04:18 +1000

    Added Week 10 code

---
+++ week10/starter/command_line_arguments.c
@@ -0,0 +1,22 @@
+// Exploration of command line arguments
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    // Read the command line arguments and print them out on separate lines
+    // Eg. ./program hello world
+    // will print: "hello\nworld\n"
+
+
+
+    // Now instead of just printing them out on separate lines,
+    // print them out but convert all lowercase letters into uppercase letters
+    // Eg. ./program hello world
+    // will print: "HELLO\nWORLD\n"
+
+    
+
+    return 0;
+}
